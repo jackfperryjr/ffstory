1d2edf2d6ca841bd74840db1abf18945717d502d
jackfperryjr <jack.franklin.perryjr@gmail.com>
2019-01-25 07:48:18 -0500

    Added button and second activity. Tested on virtual device.

--- app/src/main/java/com/jackperryjr/ffstory/MainActivity.kt
+++ app/src/main/java/com/jackperryjr/ffstory/MainActivity.kt
@@ -2,11 +2,21 @@ package com.jackperryjr.ffstory
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var startButton = findViewById(R.id.start_button) as Button
+
+        startButton.setOnClickListener(View.OnClickListener { view ->
+            val intent = Intent(view.context, Main2Activity::class.java)
+            view.context.startActivity(intent)
+        })
     }
 }
